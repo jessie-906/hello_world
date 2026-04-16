429fce76752473d6c3710690f90e2cf646d9f001
jessie-906 <cbobo7237@gmail.com>
2026-04-16 12:53:18 +0800

    alien_invasion_settings

---
+++ projects/AlienInvasion/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """储存游戏《外星人入侵》中所有设置的类"""
+    def __init__(self):
+        """初始化游戏的设置"""
+        self.screen_width = 800
+        self.screen_height = 1000
+        self.bg_color = (3,0,39)
+        self.ship_speed = 2.0
